7d71e5be80d2ea05db3735ac1dec3177af51010c
Vu TranLy <vutransingapore@gmail.com>
2017-01-31 21:34:45 +0800

    .

--- task_04/solution/hello.c
+++ task_04/solution/hello.c
@@ -1,26 +1,15 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-<<<<<<< HEAD
-int init_module(void) 
+
+int init_module(void)
 {	
 	printk(KERN_DEBUG "Hello Worlds.\n");
 	return 0;
 }
 
-void cleanup_module(void) 
+void cleanup_module(void)
 {
 	printk(KERN_DEBUG "Goodbye world.\n");
 }
-=======
-int init_module(void)
-{
-	pr_debug("Hello Worlds.\n");
-	return 0;
-}
 
-void cleanup_module(void)
-{
-	pr_debug("Goodbye world.\n");
-}
->>>>>>> 538d67f76d4f3803947180d25236fb0b41989b2a
