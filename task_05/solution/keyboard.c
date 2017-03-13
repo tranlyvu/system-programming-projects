02f9e6f990eff6802ddace746bd5f1a3aa088dd6
Tran Ly Vu <vutransingapore@gmail.com>
2017-03-13 12:34:51 +0800

    add task 5

--- task_05/solution/keyboard.c
+++ task_05/solution/keyboard.c
@@ -1,37 +1,36 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/init.h>		/* Needed for macros */
+#include <linux/hid.h>
+#include <linux/usb.h>
+
+#define DRIVER_AUTHOR "Tran Ly Vu <vutransingapore@gmail.com>"
+#define DRIVER_DESC "usb keyboard module"
+#define DRIVER_LICENSE "GPL"
+
+static int __init usb_keyboard_init(void)
+{
+	printk(KERN_INFO "Hello\n");
+
+	return 0;
+}
 
 
-static struct usb_device_id kb_table [] = {
-        { USB_DEVICE(USB_KB_VENDOR_ID, USB_KB_PRODUCT_ID) },
-        { }                      /* Terminating entry */
+static void __exit usb_keyboard_exit(void)
+{
+        printk(KERN_INFO  "Goodbye\n");
+}
+
+static struct usb_device_id usb_keyboard_id_table[] = {
+	{ USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID, USB_INTERFACE_SUBCLASS_BOOT, USB_INTERFACE_PROTOCOL_KEYBOARD) },
+	{}
 };
 
-MODULE_DEVICE_TABLE (usb, kb_table);
+MODULE_DEVICE_TABLE(usb, usb_keyboard_id_table);
 
-static int __init usb_kb_init(void)
-{
-	int result;
+module_init(usb_keyboard_init);
+module_exit(usb_keyboard_exit);
 
-	/* register this driver with the USB subsystem */
-	result = usb_register(&kb_driver);
-
-	if (result) 
-		err("usb_register failed. Error number %d", result);
-
-	return result;
-}
-
-
-static void __exit usb_kb_exit(void)
-{
-        /* deregister this driver with the USB subsystem */
-        usb_deregister(&kb_driver);
-}
-
-module_exit(usb_kb_exit);
-module_init(usb_kb_init);
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Tran Ly Vu");
-MODULE_DESCRIPTION("keyboard");
+MODULE_LICENSE(DRIVER_LICENSE);
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
