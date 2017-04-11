cf26db2572ba0ddb09aadbae36124f3d71ce16e0
Tran Ly Vu <vutransingapore@gmail.com>
2017-04-11 22:58:47 +0800

    fix task 06

--- task_06/solution/eudyptula.c
+++ task_06/solution/eudyptula.c
@@ -1,9 +1,9 @@
-#include <linux/init.h>           // Macros used to mark up functions e.g. __init __exit
-#include <linux/module.h>         // Core header for loading LKMs into the kernel
-#include <linux/device.h>         // Header to support the kernel Driver Model
-#include <linux/kernel.h>         // Contains types, macros, functions for the kernel
-#include <linux/fs.h>             // Header for the Linux file system support
-#include <asm/uaccess.h>          // Required for the copy to user function
+#include <linux/init.h>           
+#include <linux/module.h>        
+#include <linux/device.h>         
+#include <linux/kernel.h>       
+#include <linux/fs.h>             
+#include <asm/uaccess.h>          
 
 #define CLASS_NAME "eudyptula"
 #define DEVICE_NAME "eudyptula"
